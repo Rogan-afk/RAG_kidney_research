8082e6b048deb89773544e92faa1bc860d4d6fac
Rogan-afk <65251539+Rogan-afk@users.noreply.github.com>
2025-02-16 18:55:47 -0500

    Update Rag_pipeline_dep.py

--- Rag_pipeline_dep.py
+++ Rag_pipeline_dep.py
@@ -22,15 +22,25 @@ import chromadb  # Importing chromadb to access its functionalities
 # Load environment variables
 load_dotenv()
 
+#For streamlit deployment
+S3_BUCKET_NAME = st.secrets["S3_BUCKET_NAME"]
+os.environ["AWS_ACCESS_KEY_ID"] = st.secrets["AWS_ACCESS_KEY_ID"]
+os.environ["AWS_SECRET_ACCESS_KEY"] = st.secrets["AWS_SECRET_ACCESS_KEY"]
+os.environ["AWS_REGION"] = st.secrets["AWS_REGION"]
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+os.environ["GROQ_API_KEY"] = st.secrets["GROQ_API_KEY"]
+os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
+
 # Set environment variables
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
 if S3_BUCKET_NAME is None:
     raise ValueError("S3_BUCKET_NAME is not defined in the environment variables.")
 
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
-os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2")
+# os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+# os.environ["GROQ_API_KEY"] = os.getenv("GROQ_API_KEY")
+# os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
+# os.environ["LANGCHAIN_TRACING_V2"] = os.getenv("LANGCHAIN_TRACING_V2")
 
 # Initialize S3 client
 s3_client = boto3.client(
